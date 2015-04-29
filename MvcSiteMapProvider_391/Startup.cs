b55f24f524d71a8332e2e57bb1053d5977e95c0b
Shad Storhaug <shad@shadstorhaug.com>
2015-04-29 21:33:57 +0700

    Added project for MVC5

---
+++ MvcSiteMapProvider_391/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MvcSiteMapProvider_391.Startup))]
+namespace MvcSiteMapProvider_391
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
